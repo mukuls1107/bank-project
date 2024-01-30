f95e39ece6fca1c2b2d63a4cc34fd9a794e93388
mukul1107 <mukulsingh3078@gmail.com>
2024-01-30 12:41:34 +0530

    Fix a bug

--- main.cpp
+++ main.cpp
@@ -1,12 +1,11 @@
 #include <iostream>
 #include <string>
-using namespace std;
+#include <windows.h>
 
-static int accPos = 0;
+using namespace std;
 
 class BankAccount
 {
-
 private:
     int acc_number[100];
     string balance[100];
@@ -14,21 +13,24 @@ private:
 
 public:
     string acc_holder_name[100];
+    static int accPos; // Declare accPos as a static member variable
 
     void createAccount();
     void withdrawAmt(int amount);
     void depositAmt();
 };
 
-void BankAccount ::createAccount()
-{
+// Define accPos outside the class definition
+int BankAccount::accPos = 0;
 
+void BankAccount::createAccount()
+{
     string name, account_type;
     char type;
     int startingAmt;
 
     cout << "Enter your name: " << endl;
-    cin >> name;
+    getline(cin, name);
     cout << "Choose Account type ==>\nType [C] for Current Account.\nType [S] for Savings Account." << endl;
     cin >> type;
     switch (type)
@@ -45,36 +47,48 @@ void BankAccount ::createAccount()
         cout << "Invalid Choice!" << endl;
         break;
     }
+
     int id = accPos;
     acc_number[accPos] = id;
-    accPos = accPos + 1;
 
     cout << "Welcome " << name << "!" << endl
          << "Your " << account_type << " account has been created.\n"
          << "Your Account Number is:\t" << id << endl;
+    
+    accPos++; // Increment accPos after creating an account
 }
 
-void BankAccount ::depositAmt()
+void BankAccount::depositAmt()
 {
     int amount, id;
     cout << "Enter your ID: " << endl;
     cin >> id;
-
+    cout << "Searching your account";
+    Sleep(2000);
+    cout << ".";
+    Sleep(2000);
+    cout << ".";
+    Sleep(2000);
+    cout << "." << endl;
+    Sleep(2000);
     string name = acc_holder_name[id];
     cout << "Enter amount to deposit: " << endl;
     cin >> amount;
-    balance[accPos] = amount;
-    name = acc_holder_name[accPos];
-    cout << "Added " << amount << " to" << name << "'s account." << endl;
+    balance[id] = to_string(amount); // Convert amount to string for balance
+    name = acc_holder_name[id];
+    cout << "Added " << amount << " to " << acc_holder_name[id] << "'s account." << endl;
+    Sleep(3000);
+    system("cls");
 }
 
-void BankAccount ::withdrawAmt(int amount)
+void BankAccount::withdrawAmt(int amount)
 {
     int accNum, amtToDraw;
     cout << "Enter your Account Number: " << endl;
     cin >> accNum;
     cout << "Enter amount withdraw: " << endl;
     cin >> amtToDraw;
+
     cout << "Successfully withdrawn $" << amtToDraw << "From your Account." << endl;
 }
 
@@ -83,7 +97,8 @@ int main()
     BankAccount Hari;
 
     Hari.createAccount();
+    Sleep(2000);
     Hari.depositAmt();
-    Hari.withdrawAmt(350);
+    // Hari.withdrawAmt(350);
     return 0;
 }
